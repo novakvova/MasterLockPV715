6d78d0a5d7814c9724b46bac72b132a60db7739f
nikitakaida <supernikd@gmail.com>
2020-04-13 20:48:25 +0300

    add user sorting

--- MasterLock/Controllers/UserController.cs
+++ MasterLock/Controllers/UserController.cs
@@ -18,11 +18,33 @@ namespace MasterLock.Controllers
         {
             _context = context;
         }
-        [HttpGet]
-        public IActionResult Get()
+        [HttpPost]
+        public IActionResult Get([FromBody]UserSortingModel sorting)
         {
-            var model = _context.Users.Select(a => a).AsQueryable();
-            return Ok(model);
+            var users = _context.Users.Select(a => a).AsQueryable();
+            if (string.Compare("phone",sorting.Type) == 0)
+            {
+                users = _context.Users.Select(a => a).Where(a=> string.Compare(a.PhoneNumber,sorting.Value) == 0).AsQueryable();
+            }
+            else if (string.Compare("name", sorting.Type) == 0)
+            {
+                string name = sorting.Value.Split(' ')[0];
+                string surname = sorting.Value.Split(' ')[1];
+
+                users = users.Select(a => a).Where
+                (a => string.Compare(a.Name.ToLower(), name.ToLower()) == 0 
+                && string.Compare(a.Surname.ToLower(), surname.ToLower()) == 0)
+                .AsQueryable();
+            }
+            else if (string.Compare("role", sorting.Type) == 0)
+            {
+             //   users = _context.Users.Select(a => a).Where(a => string.Compare(a.StringRole.ToLower(), sorting.Value.ToLower())== 0).AsQueryable();
+            }
+            else if(string.Compare("date", sorting.Type) == 0)
+            { 
+                users = _context.Users.Select(a => a).Where(a => string.Compare(a.Date.ToLower(), sorting.Value.ToLower()) == 0).AsQueryable();
+            }
+            return Ok(users);
         }
     }
 }
---
+++ MasterLock/Entities/UserSortingModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MasterLock.Entities
+{
+    public class UserSortingModel
+    {
+        [Required]
+        public string Type { get; set; }
+        [Required]
+        public string Value { get; set; }
+
+    }
+}
